6c5cdefed769d4d9ce45ba4f3bd8039d105c45df
ArjenCole <jinhesky@hotmail.com>
2019-03-19 20:19:04 +0800

    完成sha512与原c#代码运算方式兼容

--- missions/views.py
+++ missions/views.py
@@ -1,24 +1,39 @@
 from django.shortcuts import render
 from missions import db
 import hashlib
-
+import base64
 # Create your views here.
 
 
 def index(request):
     # data = db.query("Password", "staffs", "Account", "jinhe@smedi.com")
-
     if request.method == 'POST':
         account = request.POST.get("account", None)
         password = request.POST.get("pass", None)
-        print(account)
-        print(password)
+        # print(account)
+        # print(password)
         a = (account + password)
-        b = a.encode(encoding='gb2312', errors = 'strict')
-        print(b)
-        print(hashlib.sha512(b).hexdigest())
-        return render(request, "gantt.html")
+        d = encode(a)
+        '''
+        b = bytes(a, encoding='gb2312')
+        h = hashlib.sha512(b).digest()
+        c = base64.b64encode(h)
+        d = str(c, encoding='gb2312')
+        '''
+        password = db.query("Password", "staffs", "Account", account)
+        print(password[0][0])
+        print(d == password[0][0])
+        if d == password[0][0]:
+            return render(request, "gantt.html")
+        else:
+            return render(request, "index.html")
     else:
         return render(request, "index.html")
 
 
+def encode(s):
+    b = bytes(s, encoding='gb2312')
+    h = hashlib.sha512(b).digest()
+    c = base64.b64encode(h)
+    d = str(c, encoding='gb2312')
+    return d
