27b4353b0e1a8833ab0d1e28422eec707c2c6fba
ArjenCole <jinhesky@hotmail.com>
2019-03-22 14:15:36 +0800

    甘特页面增加页眉页脚 甘特图显示自动跳转到今天，疑似未遂

--- missions/views.py
+++ missions/views.py
@@ -75,8 +75,15 @@ def getValues(t):
 
 
 def timeDiffer(date1, date2="1970-1-1"):
-    date1 = time.strptime(date1.replace("/", "-") + " 00:00:00", "%Y-%m-%d %H:%M:%S")
-    date2 = time.strptime(date2.replace("/", "-") + " 00:00:00", "%Y-%m-%d %H:%M:%S")
+    date1Replace = date1.replace("/", "-") + " 00:00:00"
+    date2Replace = date2.replace("/", "-") + " 00:00:00"
+    if str(date1Replace) == " 00:00:00":
+        date1Replace = "1970-1-1 00:00:00"
+        print("e1" + date1Replace + "dd")
+    if str(date2Replace) == " 00:00:00":
+        date2Replace = "1970-1-1 00:00:00"
+    date1 = time.strptime(date1Replace, "%Y-%m-%d %H:%M:%S")
+    date2 = time.strptime(date2Replace, "%Y-%m-%d %H:%M:%S")
     date1 = datetime.datetime(date1[0], date1[1], date1[2], date1[3], date1[4], date1[5])
     date2 = datetime.datetime(date2[0], date2[1], date2[2], date2[3], date2[4], date2[5])
     return (date1-date2).days
