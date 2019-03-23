18eccda30c7267236456c7073d89ea07d32ef896
ArjenCole <jinhesky@hotmail.com>
2019-03-23 13:45:12 +0800

    通过json格式将数据从后台传递给前台，供事件调用

--- missions/views.py
+++ missions/views.py
@@ -4,6 +4,7 @@ import hashlib
 import base64
 import time
 import datetime
+import json
 # Create your views here.
 
 
@@ -17,7 +18,8 @@ def index(request):
                 listMissions = getMissons(account)
                 return render(request, "gantt.html", {'data': listMissions})
             else:
-                return render(request, "ganttStaffs.html")
+                listMissions = getMissons(account)
+                return render(request, "gantt.html", {'data': listMissions})
         else:
             return render(request, "index.html")
     else:
@@ -65,22 +67,66 @@ def getdict(t):
 
 
 def getValues(t):
+    # data = json.dumps(t, ensure_ascii=False)
+    data = {'name': t[0], 'project_Index': t[1], 'project_Name': t[2],
+            'project_Location': t[3], 'project_Stage': t[4],
+            'schedule': t[5], 'majorDepartment': t[6], 'major': t[7],
+            'version': t[8],
+            'date_Handover': t[9], 'date_Submit': t[10], 'date_Publish': t[11],
+            'status': t[12],
+            'project_Manager': t[13], 'technical_Director': t[14], 'designer': t[15],
+            'proofreader':t[16], 'auditor': t[17], 'executor': t[18],
+            'expDays': t[19], 'remark': t[20], 'key': t[21], 'date_Finish': t[22],
+            'workCost': t[23], 'fee': t[24], 'date_Create': t[25], 'creator': t[26]}
+    data = json.dumps(data, ensure_ascii=False)
     v = dict([('from', timeFormat(timeDiffer(t[9]))),
               ('to', timeFormat(timeDiffer(t[10]))),
-              ('label', ""),
-              ('customClass', "ganttRed")])
+              # ('desc', "123"),
+              ('label', t[4]),
+              ('dataObj', data),
+              ('customClass', getGanttColor(timeDiffer(t[10], t[9]), t[4]))])
     lv = []
     lv.append(v)
     return lv
+
+
+def getGanttColor(days, stage):
+    ganttColor = "ganttGreen"
+    if stage == "施设":
+        if days < 2:
+            ganttColor = "ganttBlue"
+        elif days < 4:
+            ganttColor = "ganttRed"
+        elif days < 6:
+            ganttColor = "ganttOrange"
+        else:
+            ganttColor = "ganttGreen"
+    elif stage == "初设":
+        if days < 1:
+            ganttColor = "ganttBlue"
+        elif days < 3:
+            ganttColor = "ganttRed"
+        elif days < 5:
+            ganttColor = "ganttOrange"
+        else:
+            ganttColor = "ganttGreen"
+    else:
+        if days < 1:
+            ganttColor = "ganttRed"
+        elif days < 2:
+            ganttColor = "ganttOrange"
+        else:
+            ganttColor = "ganttGreen"
+    return ganttColor
 
 
 def timeDiffer(date1, date2="1970-1-1"):
     date1Replace = date1.replace("/", "-") + " 00:00:00"
     date2Replace = date2.replace("/", "-") + " 00:00:00"
     if str(date1Replace) == " 00:00:00":
-        date1Replace = "2017-12-31 00:00:00"
+        date1Replace = str(datetime.date.today()) + " 00:00:00"
     if str(date2Replace) == " 00:00:00":
-        date2Replace = "2017-12-31 00:00:00"
+        date2Replace = str(datetime.date.today()) + " 00:00:00"
     date1 = time.strptime(date1Replace, "%Y-%m-%d %H:%M:%S")
     date2 = time.strptime(date2Replace, "%Y-%m-%d %H:%M:%S")
     date1 = datetime.datetime(date1[0], date1[1], date1[2], date1[3], date1[4], date1[5])
